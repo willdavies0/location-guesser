a5c956db268643fb20c98e2a2235e07a6c870b1b
willdavies0 <will@pimloc.com>
2020-10-18 16:18:10 +0100

    init of app structure. added react-navigation, themeing, and redux

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import defaultState from './defaultState';
+=======
+import { defaultState } from './defaultState';
+>>>>>>> init of app structure. added react-navigation, themeing, and redux
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
